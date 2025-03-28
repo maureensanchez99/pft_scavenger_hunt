7b38287837d88dfb63bec27ad5bad8fadc3fec54
Jp <gonzalez.jean1503@gmail.com>
2025-03-28 17:02:45 -0500

    Made it more LSU Branded and additionally, also made adjustment with the structure of the code.

--- lib/pages/01_scavenger/riddle_passage.dart
+++ lib/pages/01_scavenger/riddle_passage.dart
@@ -15,49 +15,110 @@ class _RiddlePassageState extends State<RiddlePassage> {
   
   // State for nav rail
   bool _isNavRailExtended = false;
-  final answer = TextEditingController();
+  final _answer = TextEditingController();
+  final String correctAnswer = "1344";
+  String answerMessage = "";
+  bool questionDone = false;
 
+
+  void _checkAnswer()
+  {
+    setState(() 
+    {
+      if(_answer.text == correctAnswer)
+      {
+        questionDone = true;
+        answerMessage = "You got it right! Good Job";
+      }
+      else
+      {
+        questionDone = false;
+        answerMessage = "You did not get the right answer, you fucking dumbass";
+      }
+    });
+    _showBottomCard();
+  }
+
+  void _showBottomCard()
+  {
+    showModalBottomSheet
+    (
+      context: context,
+      builder: (BuildContext context)
+      {
+        return Container
+        (
+          height: 300,
+          width: double.infinity,
+          decoration: BoxDecoration
+          (
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            gradient: LinearGradient
+            (
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: 
+              [
+                Color(0xFFFDD023), // lsuGold
+                Color(0xFF461D7C), // lsuPurple
+              ],
+            ),
+          ),
+          child: Center
+          (
+            child: Column
+            (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: 
+              [
+                Text
+                (
+                  answerMessage,
+                  style: TextStyle
+                  (
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+                
+              ]
+            )
+          )
+        );
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: lsuPurple,
-        title: const Text(
-          'Riddle Passage',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            color: Color(0xFFFDD023),
-          ),
-        ),
-        centerTitle: true,
-      ),
       body: Stack(
         children: [
           // Main content
           Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFFFDD023), // lsuGold
-                  Color(0xFF461D7C), // lsuPurple
-                ],
-              ),
+            decoration: const BoxDecoration
+            (
+              color:Colors.white
             ),
-            child: Column(
-              children: [
+            child: Column
+            (
+              children: 
+              [
                 // Hamburger menu at the top left
                 Align(
                   alignment: Alignment.topLeft,
-                  child: Padding(
+                  child: Padding
+                  (
                     padding: const EdgeInsets.only(left: 16.0, top: 16.0),
-                    child: IconButton(
+                    child: IconButton
+                    (
                       icon: const Icon(Icons.menu, color: lsuPurple),
-                      onPressed: () {
-                        setState(() {
+                      onPressed: () 
+                      {
+                        setState(() 
+                        {
                           _isNavRailExtended = !_isNavRailExtended;
                         });
                       },
@@ -66,54 +127,110 @@ class _RiddlePassageState extends State<RiddlePassage> {
                 ),
                 
                 // Main content
-                Expanded(
-                  child: SingleChildScrollView(
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "As we begin the hunt, you need to find where Jp went",
-                            textAlign: TextAlign.center,
-                            style: TextStyle
-                            (
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold, 
-                              
-                              color: Colors.white
+                Expanded
+                (
+                  child: SingleChildScrollView
+                  (
+                    child: Center
+                    (
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children:
+                          [
+                            Text(
+                              "As we begin the hunt, you need to find where Jp went",
+                              textAlign: TextAlign.center,
+                              style: TextStyle
+                              (
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold, 
+                                
+                                color: lsuPurple
+                              ),
                             ),
-                          ),
-                          Image.asset("assets/Jpnote.PNG"),
-                          TextField
-                          (
-                            controller: answer,
-                            decoration: InputDecoration
-                            (
-                              border: OutlineInputBorder
+                            Image.asset("assets/Jpnote.PNG"),
+                            SizedBox(height:20),
+                            if (!questionDone)
+                              TextField
+                              (
+                                textAlign: TextAlign.center,
+                                style: TextStyle
                                 (
-                                  borderRadius: BorderRadius.circular(10)
+                                  fontSize: 20
                                 ),
-                              fillColor: Colors.white,
-                              hintText: "What is the room Jp is always in?",
-                              hintStyle: TextStyle
+                                controller: _answer,
+                                decoration: InputDecoration
                                 (
-                                  color: Colors.white,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold
+                                  border: OutlineInputBorder
+                                    (
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  filled: true,
+                                  fillColor: Color(0xFFA39AAC),
+                                  hintText: "What is the room Jp is always in?",
+                                  hintStyle: TextStyle
+                                    (
+                                      color: lsuPurple,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold
+                                    ),
                                 ),
+                              ),
+                              SizedBox(height: 18),
+                            if(!questionDone)
+                              ElevatedButton
+                              (
+                                onPressed: _checkAnswer, 
+                                style: ElevatedButton.styleFrom
+                                (
+                                  backgroundColor: lsuGold
+                                ),
+                                child: 
+                                  Text
+                                  (
+                                    "Check",
+                                    style: TextStyle
+                                    (
+                                      fontSize: 15,
+                                      color: lsuPurple
+                                    ),
+                                  )
+                                    
+                                  
+                              )
                               
-                            ),
-                            
-
-                          )
-
-                        ],
-                      ),
+                            else
+                              SizedBox
+                              (
+                                height: 50,
+                                width: 100,
+                                child:
+                                (
+                                  DecoratedBox
+                                  (
+                                    decoration: BoxDecoration
+                                    (
+                                      borderRadius: BorderRadius.circular(40),
+                                      color: Colors.green,
+                                    ),
+                                    child:
+                                    (
+                                      Icon
+                                      (
+                                        Icons.check,
+                                        color: Colors.white
+                                      )
+                                    )
+                                  )
+                                )
+                              )
+                          ], //children
+                        ),
+                      ), 
                     ),
                   ),
-                ),
                 ),
               ],
             ),
