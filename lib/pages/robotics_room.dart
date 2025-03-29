4a2ecaca04af2b023ca6e9dbab83d2a521c47a7f
Trellis246 <113363844+Trellis246@users.noreply.github.com>
2025-03-28 23:13:10 -0500

    removed gradient

--- lib/pages/robotics_room.dart
+++ lib/pages/robotics_room.dart
@@ -8,27 +8,10 @@ class RoboticsRoom extends StatefulWidget {
   State<RoboticsRoom> createState() => _RoboticsRoomState();
 }
 
-class _RoboticsRoomState extends State<RoboticsRoom> with SingleTickerProviderStateMixin {
+class _RoboticsRoomState extends State<RoboticsRoom> {
   // LSU colors
   static const Color lsuPurple = Color(0xFF461D7C); // LSU Purple
   static const Color lsuGold = Color(0xFFFDD023);   // LSU Gold
-  
-  late AnimationController _animationController;
-  
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    )..repeat(reverse: true);
-  }
-  
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,30 +32,15 @@ class _RoboticsRoomState extends State<RoboticsRoom> with SingleTickerProviderSt
         children: [
           const ScavengerHuntNavRail(selectedIndex: 10),
           Expanded(
-            child: AnimatedBuilder(
-              animation: _animationController,
-              builder: (context, child) {
-                return Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color.lerp(lsuGold, lsuPurple, _animationController.value) ?? lsuGold,
-                        Color.lerp(lsuPurple, lsuGold, _animationController.value) ?? lsuPurple,
-                      ],
-                    ),
-                  ),
-                  child: child,
-                );
-              },
+            child: Container(
+              color: lsuPurple,
               child: Center(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
+                    children: const [
+                      Text(
                         'Robotics Room Content',
                         style: TextStyle(
                           fontSize: 24,
