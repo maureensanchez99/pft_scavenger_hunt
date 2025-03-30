d881de5d6bbc8212b7a96e77091f88f9dcf0ffc2
Jp <gonzalez.jean1503@gmail.com>
2025-03-30 00:16:19 -0500

    Added A hard to read that would put the note text in a alert box

--- lib/pages/01_scavenger/riddle_passage.dart
+++ lib/pages/01_scavenger/riddle_passage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../widgets/nav_rail.dart';
+// import '../assets/fonts';
 
 class RiddlePassage extends StatefulWidget {
   const RiddlePassage({super.key});
@@ -91,6 +92,81 @@ class _RiddlePassageState extends State<RiddlePassage> {
     );
   }
 
+  void _biggerNote()
+  {
+    showDialog
+    (
+      context: context,
+      builder: (BuildContext context)
+      {
+        return AlertDialog
+        (
+          title: Text
+          (
+            textAlign: TextAlign.center,
+            "Jp's Note"
+          ),
+          content: Expanded
+            (
+              child: SingleChildScrollView
+              (
+                child: Center
+                (
+                  child: Column
+                  (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                    [
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "Hey, Person, I apologize but I needed some AA batteries. However, if you can find me you can have them back. You just need to put in below the specific room number that I am in or else the batteries are HHAHAHAHAHA"
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nAnyways, because I'm a caring guy, I left you some notes on where to find me."
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\n\nLet's see, where did I start if not only the only palce that we all end when it comes to our shared learning."
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nI remember taking a left since it was then the only right thing to do before the water fountain."
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nThen I aimlessly walked until I reached a fork in the road, or maybe I should say a cross, HAHAHAHA, anyways, I was tired so I needed a place to sit down for a minute and relax as if I was near an Oasis"
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nAfterword, I got up deciding to go the direction I started until I reach a place with high ceilings."
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nWhen I kept walking, I reached some puertas, then went in the direction of a place that looked familair until I past a place with not money, but the other type and reached a place one can hear a ding now but this time a year ago."                
+                      ),
+                      Text
+                      (
+                        textAlign: TextAlign.center,
+                        "\nI then took a right until I reach a palce that I feel welcomed, Somewhere where I can relax as if I am a raft floating in the sea."
+                      )
+                    ]
+                  ),
+                )
+              )
+            )
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -139,7 +215,27 @@ class _RiddlePassageState extends State<RiddlePassage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children:
                           [
-                            Text(
+                            Title
+                            (
+                              color: lsuPurple, 
+                              child: Text
+                              (
+                                "Riddle Passage",
+                                textAlign: TextAlign.center,
+                                style: TextStyle
+                                (
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              )
+                            ),
+                            SizedBox
+                            (
+                              height: 50
+                            ),
+                            Text
+                            (
                               "As we begin the hunt, you need to find where Jp went",
                               textAlign: TextAlign.center,
                               style: TextStyle
@@ -151,6 +247,24 @@ class _RiddlePassageState extends State<RiddlePassage> {
                               ),
                             ),
                             Image.asset("assets/Jpnote.PNG"),
+                            SizedBox(height:20),
+                            ElevatedButton
+                            (
+                              onPressed: _biggerNote,
+                              style: ElevatedButton.styleFrom
+                              (
+                                backgroundColor: Color(0xFF3C1053)
+                              ),
+                              child: Text
+                              (
+                                "Hard to Read?",
+                                style: TextStyle
+                                (
+                                  color: Colors.white,
+                                  fontSize: 12
+                                ),
+                              )
+                            ),
                             SizedBox(height:20),
                             if (!questionDone)
                               TextField
@@ -200,7 +314,6 @@ class _RiddlePassageState extends State<RiddlePassage> {
                                     
                                   
                               )
-                              
                             else
                               SizedBox
                               (
